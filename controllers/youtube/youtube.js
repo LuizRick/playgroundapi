6df54e3e07e648a43ec4367892de5fa8906e3176
Luiz Henrique Monteiro Silva De Lima <luizmonteiro@brq.com>
2019-08-30 14:18:53 -0300

    add ffmpeg suport, and video youtube convert to audio

--- controllers/youtube/youtube.js
+++ controllers/youtube/youtube.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const router = express.Router();
 const ytdlcore = require('ytdl-core');
-
-
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
 
 router.get("/get-yt-video-info", (req, res) => {
     const youtubeUrl = req.query.yt_url;
     const videoId = youtubeUrl.split("?")[1].match(/v=([^&]+)/)[1];
     const VIDEO_URL = "https://www.youtube.com/get_video_info?video_id=" + videoId;
     const video_metadata = {};
-    axios.get(VIDEO_URL).then( response => {
-        const get_video_info =  qsToJson(response.data);
+    axios.get(VIDEO_URL).then(response => {
+        const get_video_info = qsToJson(response.data);
 
         let tmp = get_video_info["url_encoded_fmt_stream_map"];
-        if(tmp){
+        if (tmp) {
             tmp = tmp.split(",")
-            for(let i in tmp){
+            for (let i in tmp) {
                 tmp[i] = qsToJson(tmp[i])
                 tmp[i].ext = tmp[i].type
-                                    .match(/^video\/\w+(?=;)/g)[0]
-                                    .replace(/^video\//, "");
+                    .match(/^video\/\w+(?=;)/g)[0]
+                    .replace(/^video\//, "");
             }
 
             video_metadata["videos"] = tmp;
@@ -34,11 +34,11 @@ router.get("/get-yt-video-info", (req, res) => {
 })
 
 
-router.get("/yt-info", (req,res) => {
+router.get("/yt-info", (req, res) => {
     const youtubeUrl = req.query.yt_url;
     const videoId = youtubeUrl.split("?")[1].match(/v=([^&]+)/)[1];
     ytdlcore.getInfo(videoId, (err, info) => {
-        if(err) res.send(err)
+        if (err) res.send(err)
         else res.send(info)
     })
 })
@@ -48,26 +48,37 @@ router.get('/yt-download', (req, res) => {
     const youtubeUrl = req.query.yt_url;
     const quality = req.query.quality;
     const name = req.query.name || "video-youtube";
-    
-    res.header('Content-Disposition', `attachment; filename="${name}.mp4"`);
-    ytdlcore(youtubeUrl,{
+
+    const stream = ytdlcore(youtubeUrl, {
         format: 'mp4',
         quality
-    }).pipe(res);
+    });
+
+    if (quality.indexOf("audio") != -1) {
+        res.header('Content-Disposition', `attachment; filename="${name}.mp3"`);
+        const proc = new ffmpeg({ source: stream });
+        proc.withAudioCodec('libmp3lame')
+            .toFormat('mp3')
+            .output(res)
+            .run();
+    } else {
+        res.header('Content-Disposition', `attachment; filename="${name}.mp4"`);
+        stream.pipe(res);
+    }
 })
 
 
 function qsToJson(queryString) {
-	let res = {};
-	let params = queryString.split("&");
-	let keyValuePair, key, value;
-	for (let i in params) {
-		keyValuePair = params[i].split("=");
-		key = keyValuePair[0];
-		value = keyValuePair[1];
-		res[key] = decodeURIComponent(value);
-	}
-	return res;
+    let res = {};
+    let params = queryString.split("&");
+    let keyValuePair, key, value;
+    for (let i in params) {
+        keyValuePair = params[i].split("=");
+        key = keyValuePair[0];
+        value = keyValuePair[1];
+        res[key] = decodeURIComponent(value);
+    }
+    return res;
 }
 
 module.exports = router;
