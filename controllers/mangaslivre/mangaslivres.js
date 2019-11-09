8ec2673d4e41698324259e53838ca4caa43adfbf
Luiz henrique <luizmon.silva@gmail.com>
2019-11-08 22:25:24 -0300

    adicionando end point para favoritos

--- controllers/mangaslivre/mangaslivres.js
+++ controllers/mangaslivre/mangaslivres.js
@@ -2,33 +2,72 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 const mongodb = require('mongodb')
+const url = "mongodb://localhost:27017/"
 
-router.post(`/mangaslivre`, (req,res) => {
+router.post(`/mangaslivre`, (req, res) => {
     const data = req.body.BodyRequest
     const options = {
-        method:'POST',
-        url:req.body.url,
+        method: 'POST',
+        url: req.body.url,
         data
-   }
-
-    if(req.body.RequestType == 0 || req.body.RequestType == 1){
-         options["headers"] = { "X-Requested-With" : "XMLHttpRequest", 'content-type' : 'application/json' }
     }
 
-   axios(options).then(response => {
-       res.send(response.data)
-   }).catch(err => {
-       res.send('erro')
-   })
+    if (req.body.RequestType == 0 || req.body.RequestType == 1) {
+        options["headers"] = { "X-Requested-With": "XMLHttpRequest", 'content-type': 'application/json' }
+    }
+
+    axios(options).then(response => {
+        res.send(response.data)
+    }).catch(err => {
+        res.send('erro')
+    })
 })
 
-router.post(`/save-manga-info`, (req, res) => {
+router.post(`/saveMangaFavorito`, (req, res) => {
     const MongoClient = mongodb.MongoClient
-    const url = "mongodb://localhost:27017/"
+
+    const collection = "mangasfavoritos", databasename = "playground";
     MongoClient.connect(url, (err, db) => {
-        if(err) throw err;
-        db.close()
+        if (err) res.send({
+            status: 500,
+            resultado: err.message
+        })
+        let dbo = db.db(databasename);
+        dbo.collection(collection).insertOne(req.body.manga, (err, result) => {
+            if (err) res.send({
+                status: 500,
+                resultado: err.message
+            })
+            else res.send({
+                status: 200,
+                resultado: result.insertedCount
+            })
+        })
+        db.close();
     })
- })
+})
+
+router.get('/getMangasFavoritos', (req, res) => {
+    const MongoClient = mongodb.MongoClient
+    const collection = "mangasfavoritos", databasename = "playground";
+    MongoClient.connect(url, (err, db) => {
+        if (err) res.send({
+            status: 500,
+            resultado: err.message
+        })
+        let dbo = db.db(databasename);
+        dbo.collection(collection).find({}).toArray((err, result) => {
+            if (err) res.send({
+                status: 500,
+                resultado: err.message
+            })
+            else res.send({
+                status: 200,
+                resultado: result
+            })
+        })
+        db.close();
+    });
+})
 
 module.exports = router;
